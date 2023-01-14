5152b79c20d5b11b33468fe2b15a725956275ca6
Jack Murphy <johnjmur0@gmail.com>
2023-01-14 17:13:28 -0500

    wip out proc

--- src/stoch_process_base.py
+++ src/stoch_process_base.py
@@ -1,9 +1,10 @@
-from typing import Optional
-
+from typing import Optional, Union
+from dataclasses import dataclass
+from sklearn.linear_model import LinearRegression
 import numpy as np
 
 
-class Stochastic_Process:
+class Brownian_Motion:
     def __init__(self, seed=None):
         self.generator = np.random.default_rng(seed=seed)
 
@@ -13,7 +14,7 @@ class Stochastic_Process:
 
     def get_W(self, intervals: int) -> np.ndarray:
 
-        dW = Stochastic_Process.get_dW(self, intervals)
+        dW = self.get_dW(intervals)
         dW_cs = dW.cumsum()
         return dW_cs
 
@@ -43,10 +44,107 @@ class Stochastic_Process:
             else:
                 dW_corr_ref = self._get_corr_ref_dW(dWs, i)
                 dW_i = self._get_correlated_dW(dW_corr_ref, rho)
+
             dWs.append(dW_i)
+
         return np.asarray(dWs).T
 
     def _get_corr_ref_dW(self, dWs: list[np.ndarray], i: int) -> np.ndarray:
 
         random_proc_idx = self.generator.choice(i)
         return dWs[random_proc_idx]
+
+
+@dataclass
+class OU_Params:
+
+    mean_reversion: float
+    asymptotic_mean: float
+    std_dev: float
+
+
+class OU_Process:
+    def get_OU_process(
+        self,
+        intervals: int,
+        OU_params: OU_Params,
+        dW: np.array,
+        X_0: Optional[float] = None,
+    ) -> np.array:
+
+        interval_arr = np.arange(intervals, dtype=np.longdouble)
+        exp_alpha_t = np.exp(-OU_params.mean_reversion * interval_arr)
+
+        integral_W = OU_Process._get_integal_W(interval_arr, dW, OU_params)
+        _X_0 = OU_Process._select_X_0(X_0, OU_params)
+
+        return (
+            _X_0 * exp_alpha_t
+            + OU_params.asymptotic_mean * (1 - exp_alpha_t)
+            + OU_params.std_dev * exp_alpha_t * integral_W
+        )
+
+    def estimate_OU_params(self, X_t: np.ndarray) -> OU_Params:
+
+        y = np.diff(X_t)
+        X = X_t[:-1].reshape(-1, 1)
+        reg = LinearRegression(fit_intercept=True)
+        reg.fit(X, y)
+
+        alpha = -reg.coef_[0]
+        gamma = reg.intercept_ / alpha
+
+        y_hat = reg.predict(X)
+        beta = np.std(y - y_hat)
+
+        return OU_Params(mean_reversion=alpha, asymptotic_mean=gamma, std_dev=beta)
+
+    def get_corr_OU_procs(
+        self,
+        intervals: int,
+        OU_params: Union[OU_Params, tuple[OU_Params, ...]],
+        brownian_motion_instance: Brownian_Motion,
+        n_procs: Optional[int] = None,
+        proc_correlation: Optional[float] = None,
+    ) -> np.ndarray:
+
+        _n_procs = self._get_n_procs(OU_params, n_procs)
+        corr_dWs = brownian_motion_instance.get_corr_dW_matrix(
+            intervals, _n_procs, proc_correlation
+        )
+
+        OU_procs = []
+        for i in range(_n_procs):
+
+            if isinstance(OU_Params, tuple):
+                OU_params_i = OU_params[i]
+            else:
+                OU_params_i = OU_params
+
+            dW_i = corr_dWs[:, i]
+            OU_procs.append(self.get_OU_process(intervals, OU_params_i, dW_i))
+
+        return np.asarray(OU_procs).T
+
+    def _get_n_procs(
+        self, OU_params: Union[OU_Params, tuple[OU_Params, ...]], n_procs: Optional[int]
+    ) -> int:
+
+        if isinstance(OU_params, tuple):
+            return len(OU_params)
+        elif n_procs is None:
+            raise ValueError("If OU_params is not tuple, n_procs must be specified")
+        return n_procs
+
+    def _select_X_0(X_0_in: Optional[float], OU_params: OU_Params) -> float:
+        if X_0_in is not None:
+            return X_0_in
+        return OU_params.asymptotic_mean
+
+    def _get_integal_W(
+        intervals: np.ndarray, dW: np.ndarray, OU_params: OU_Params
+    ) -> np.ndarray:
+
+        exp_alpha_s = np.exp(OU_params.mean_reversion * intervals)
+        integral_W = np.cumsum(exp_alpha_s * dW)
+        return integral_W
--- tests/test_stoch_process.py
+++ tests/test_stoch_process.py
@@ -1,12 +1,13 @@
 import numpy as np
+import pandas as pd
 import pytest
-from src.stoch_process_base import Stochastic_Process
+from src.stoch_process_base import Brownian_Motion, OU_Process, OU_Params
 
 
-class Test_Stochastic_Process:
+class Test_Brownian_Motion:
     def test_brownian_motion_change(self):
 
-        stoch_instance = Stochastic_Process(seed=None)
+        stoch_instance = Brownian_Motion(seed=None)
 
         process = stoch_instance.get_dW(intervals=10000)
 
@@ -16,7 +17,7 @@ class Test_Stochastic_Process:
 
     def test_brownian_motion_basic(self):
 
-        stoch_instance = Stochastic_Process(seed=None)
+        stoch_instance = Brownian_Motion(seed=None)
 
         process = stoch_instance.get_W(intervals=100)
 
@@ -26,15 +27,15 @@ class Test_Stochastic_Process:
     def test_brownian_motion_seed(self):
 
         seed_same = 10
-        stoch_instance = Stochastic_Process(seed=seed_same)
+        stoch_instance = Brownian_Motion(seed=seed_same)
         process_1 = stoch_instance.get_W(intervals=100)
 
-        stoch_instance_2 = Stochastic_Process(seed=seed_same)
+        stoch_instance_2 = Brownian_Motion(seed=seed_same)
         process_2 = stoch_instance_2.get_W(intervals=100)
 
         assert all(process_1 == process_2)
 
-        stoch_instance_3 = Stochastic_Process(seed=seed_same * 2)
+        stoch_instance_3 = Brownian_Motion(seed=seed_same * 2)
         process_3 = stoch_instance_3.get_W(intervals=100)
 
         assert process_3.sum() != process_1.sum()
@@ -42,38 +43,108 @@ class Test_Stochastic_Process:
     def test_brownian_motion_corr(self):
 
         seed_same = 10
-        stoch_instance = Stochastic_Process(seed=seed_same)
+        stoch_instance = Brownian_Motion(seed=seed_same)
         org_process = stoch_instance.get_dW(intervals=100)
 
         corr_val = 0.5
-        corr_process_1 = stoch_instance.get_correlated_dW(org_process, corr_val)
+        corr_process_1 = stoch_instance._get_correlated_dW(org_process, corr_val)
 
         assert org_process.mean() != corr_process_1.mean()
 
         corr_val = 1
-        corr_process_2 = stoch_instance.get_correlated_dW(org_process, corr_val)
+        corr_process_2 = stoch_instance._get_correlated_dW(org_process, corr_val)
 
         assert all(org_process == corr_process_2)
 
         corr_val = -1
-        corr_process_3 = stoch_instance.get_correlated_dW(org_process, corr_val)
+        corr_process_3 = stoch_instance._get_correlated_dW(org_process, corr_val)
         all(org_process * -1 == corr_process_3)
 
     def test_brownian_multiple_corr(self):
 
         seed = 123
-        stoch_instance = Stochastic_Process(seed=seed)
+        stoch_instance = Brownian_Motion(seed=seed)
 
         corr_matrix_closer = stoch_instance.get_corr_dW_matrix(
-            intervals=100, n_procs=50, rho=0.75
+            intervals=100, n_procs=50, rho=0.9
         )
 
-        closer_means = [x.mean() for x in corr_matrix_closer]
+        avg_variance_closer = (
+            pd.DataFrame(np.cumsum(corr_matrix_closer, axis=0))
+            .reset_index(drop=False)
+            .melt(id_vars=["index"])
+            .groupby(["index"])
+            .var()
+            .mean()[0]
+        )
 
         corr_matrix_farther = stoch_instance.get_corr_dW_matrix(
-            intervals=100, n_procs=50, rho=0.25
+            intervals=100, n_procs=50, rho=0.1
         )
 
-        farther_means = [x.mean() for x in corr_matrix_farther]
+        avg_variance_farther = (
+            pd.DataFrame(np.cumsum(corr_matrix_farther, axis=0))
+            .reset_index(drop=False)
+            .melt(id_vars=["index"])
+            .groupby(["index"])
+            .var()
+            .mean()[0]
+        )
 
-        assert np.std(closer_means) < np.std(farther_means)
+        assert avg_variance_closer < avg_variance_farther
+
+
+class Test_OU_Process:
+    def test_get_ou_process(self):
+
+        intervals = 1000
+        ou_proc = OU_Process()
+        ou_params = OU_Params(mean_reversion=0.07, asymptotic_mean=0.01, std_dev=0.001)
+
+        brw_motion = Brownian_Motion(seed=12345)
+        dW = brw_motion.get_dW(intervals)
+
+        ou_sim = ou_proc.get_OU_process(intervals, ou_params, dW)
+
+        assert len(ou_sim) == intervals
+
+        assert ou_sim[0] == pytest.approx(ou_params.asymptotic_mean, abs=0.01)
+        assert ou_sim.mean() == pytest.approx(ou_params.asymptotic_mean, abs=0.01)
+        assert ou_sim.std() == pytest.approx(ou_params.std_dev, abs=0.01)
+
+    def test_get_ou_estimation(self):
+
+        intervals = 1000
+        ou_proc = OU_Process()
+        ou_params = OU_Params(mean_reversion=0.1, asymptotic_mean=0.2, std_dev=0.05)
+
+        brw_motion = Brownian_Motion(seed=6789)
+        dW = brw_motion.get_dW(intervals)
+
+        ou_sim = ou_proc.get_OU_process(intervals, ou_params, dW)
+
+        ou_params_est = ou_proc.estimate_OU_params(ou_sim)
+
+        assert ou_params_est.mean_reversion == pytest.approx(
+            ou_params.mean_reversion, abs=0.1
+        )
+        assert ou_params_est.asymptotic_mean == pytest.approx(
+            ou_params.asymptotic_mean, abs=0.01
+        )
+        assert ou_params_est.std_dev == pytest.approx(ou_params.std_dev, abs=0.01)
+
+    def test_ou_corr_single(self):
+
+        intervals = 1000
+        brw_motion = Brownian_Motion(seed=91234)
+        ou_proc = OU_Process()
+        ou_params = OU_Params(mean_reversion=0.4, asymptotic_mean=4, std_dev=3)
+
+        corr = 0.9
+        n_proc = 100
+
+        ou_sims = ou_proc.get_corr_OU_procs(
+            intervals, ou_params, brw_motion, n_proc, corr
+        )
+
+        assert (intervals, n_proc) == ou_sims.shape
